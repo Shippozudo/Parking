47320634c4b6866392ffb29ca2cf3716040a1a28
Denis H Bambinetti <86241457+Shippozudo@users.noreply.github.com>
2024-03-20 14:37:02 -0300

    .

---
+++ RabbitMq/Consumer/Consumer.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare
+            (
+                queue: "fila02",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            Console.WriteLine("Esperando a mensagem");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, EventArgs) =>
+            {
+                var body = EventArgs.Body.ToArray();
+                var mensagem = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Mensagem Recebida {0}", mensagem);
+            };
+
+            channel.BasicConsume
+            (
+                queue: "fila02",
+                autoAck: true,
+                consumer: consumer
+            );
+
+        }
+    }
+}
---
+++ RabbitMq/Sender/Sender.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace rabbitmq
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare
+            (
+                queue: "fila02",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            Console.WriteLine("Enviando mensagem .....");
+
+            var mensagem = "Olá esta é uma mensagem para a fila02";
+            var body = Encoding.UTF8.GetBytes(mensagem);
+
+            channel.BasicPublish
+            (
+                exchange: "",
+                routingKey: "fila02",
+                basicProperties: null,
+                body
+            );
+
+            Console.WriteLine("Mensagem enviada");
+        }
+    }
+}
